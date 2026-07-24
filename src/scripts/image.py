48663e2d709605585585800fb2ad58b9140b6107
Alex Pyrgiotis <alex.p@freedom.press>
2026-07-24 11:36:20 +0300

    Pin Buildkit image to v0.31.2
    Fix some issues in the way we were building our Dangerzone container
    image:
    1. Explicitly pin the Buildkit image for the build step of the
       `build-push-image` CI job to v0.31.2. Previously, we were
       unknowingly passing an empty string to the `docker/buildx` action,
       and it was using the latest Buildkit image it was aware of.
    2. Pin the Buildkit image to v0.31.2 when using our `image` script as
       well. Currently, it's using the pinned image in the `repro-build`
       project, which may not always be what we want.
    3. Unpin the Buildkit image when **reproducing** a Dangerzone image in
       our CI. This way, we can catch regressions in Buildkit early on,
       which is how we learned about #36.
    
    Note that so far, even though we were building our container image with
    an unpinned Buildkit image, it was producing bit-for-bit the same
    results as our reference image, which was built with Buildkit v0.23.2.
    We are therefore confident that no unexpected behavior was introduced.
    Now that we are building Dangerzone with a newer Buildkit image, which
    changes the manifest types to OCI instead of Docker, we should be on
    the lookout for new issues.
    
    Fixes #36

--- src/scripts/image.py
+++ src/scripts/image.py
@@ -36,6 +36,8 @@ CONTAINER_RUNTIME = "podman"
 ANNOTATION_DATE = "rocks.dangerzone.debian_archive_date={date}"
 CACHE_DIR = Path(tempfile.gettempdir()) / "dangerzone-reproduce-cache"
 CACHE_TTL = timedelta(hours=2)
+BUILDKIT_IMAGE = "docker.io/moby/buildkit:0.31.2@sha256:2f5adac4ecd194d9f8c10b7b5d7bceb5186853db1b26e5abd3a657af0b7e26ec"
+
 
 # NOTE: You can grab the SLSA attestation for an image/tag pair with the following
 # commands:
@@ -490,6 +492,7 @@ def build_image(
         annotation=[date_annotation],
         platform=platform,
         dry=dry,
+        buildkit_image=BUILDKIT_IMAGE,
     )
     builder.build()
 
